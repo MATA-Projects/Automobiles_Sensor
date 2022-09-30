486f489132af1ab6e4391ee976d980270f6add82
mohido <mohammed.al-mahdawi@nokai.com>
2022-09-30 15:08:44 +0200

    Basic Frontend Setup

---
+++ visualizer/server.js
@@ -0,0 +1,31 @@
+
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// ENVIRONMENT VARIABLES
+const PORT = 5000;
+const FRONTEND_URL = "http://localhost:" + PORT
+
+
+// SETTING THE RENDERING ENGINE
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'frontend/views'));
+
+// SETTING THE PUBLIC PATHS
+app.use('/js', express.static('frontend/js'));
+app.use('/css', express.static('frontend/css'));
+
+// SETTIGN THE RENDERING ENDPOINT
+app.get('/', (req, res) => {
+  return res.render('index',{
+        subject: "Automobiles Surrounding Engine", 
+        frontend_url:FRONTEND_URL
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+});
